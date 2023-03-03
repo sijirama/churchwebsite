313c6b60318b8a84a8345d54c0d153664dde6fb1
unknown <gbemilesanmi@gmail.com>
2023-03-03 03:07:26 +0100

    feature: events page

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,16 @@ function App() {
         return () => window.addEventListener('scroll' , handleScroll)
     } , [])
 
+    useEffect(() => {
+        setSelectedPage("home")
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+    }, [])
+
   return (
     <div className="app bg-deepblue">
+
       <Navbar isTopOfPage={isTopOfPage}  selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+
       <div className='w-5/6 mx-auto md:h-full'>
         {isAboveMediumScreens && ( <DotGroup selectedPage = {selectedPage} setSelectedPage = {setSelectedPage} /> )}
         <Landing setSelectedPage = {setSelectedPage} />
@@ -31,6 +38,12 @@ function App() {
       <div className='w-full bg-deepblue'>
         <div className='w-5/6 mx-auto md:h-full '>
             <Events />
+        </div>
+      </div>
+
+      <div className='w-full bg-deepblue'>
+        <div className='w-5/6 mx-auto md:h-full '>
+            <About/>
         </div>
       </div>
 
