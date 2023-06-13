d0ad26bb790bd3bd40909d360d02d796485fb455
sijibomi <gbemilesanmi@gmail.com>
2023-06-13 22:53:29 +0100

    donate page added

--- src/components/Give.tsx
+++ src/components/Give.tsx
@@ -1,17 +1,17 @@
-import Picture from '/sunset.jpg'
-
+import { RiHandHeartLine } from 'react-icons/ri'
 export default function Give() {
     return (
-        <section
-            style={{ backgroundImage: `url(${Picture})` }}
-            className="py-14 md:py-16 lg:py-20 px-14 flex flex-col gap-1 md:gap-3 items-center justify-center text-white bg-cover bg-top bg-fixed"
-        >
-            <p className="font-bold font-rubik text-lg md:text-xl lg:text-2xl">JOIN US FOR SERVICE</p>
-            <p className="font-semibold font-rubik">SUNDAYS - 8:00AM & 10:30AM</p>
-            <div className="max-w-xl text-center text-sm lg:text-sm">
-                Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat
-                Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        <section className="py-14 md:py-16 lg:py-20 px-14 flex flex-col gap-1 md:gap-3 items-center justify-center ">
+            <div>
+                <RiHandHeartLine size={80} className="text-gray-600" />
             </div>
+            <div className="max-w-xl font-bold lg:font-extrabold font-manrope text-center text-3xl md:text-4xl lg:text-5xl text-gray-600">
+                "Whoever is generous to the poor lends to the lord, and he will repay him for his deed."
+            </div>
+            <p className="text-gray-600 font-manrope text-sm md:text-base lg:text-lg my-2">Proverbs 19:16</p>
+            <button className="px-5 py-2 text-gray-600 border border-b-4 border-r-4 border-gray-600  shadow-lg active:shadow-sm active:border-b-2 active:border-r-2 tracking-widest font-bold">
+                DONATE NOW
+            </button>
         </section>
     )
 }
--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,10 +1,13 @@
 import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+import useScreenSize from '../hooks/useScreenSize'
 
 export default function Location() {
     const locationn: any = [6.437361400682429, 3.4328605354896244]
+    const screen = useScreenSize()
+    const zoom = screen === 'small' ? 12 : 17
 
     return (
-        <MapContainer center={locationn} zoom={17} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
+        <MapContainer center={locationn} zoom={zoom} scrollWheelZoom={false} style={{ height: '400px', width: '100%' }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="Map data © OpenStreetMap contributors"
