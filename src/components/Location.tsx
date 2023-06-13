36fd6b1b274bb6e3c419d4f86cdf1dd551381acb
sijibomi <gbemilesanmi@gmail.com>
2023-06-13 23:46:19 +0100

    leaflet fix

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,11 +1,20 @@
 import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
 import useScreenSize from '../hooks/useScreenSize'
-import 'leaflet/dist/images/marker-shadow.png'
-
+import 'leaflet/dist/leaflet.css'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import L from 'leaflet'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 export default function Location() {
     const locationn: any = [6.437361400682429, 3.4328605354896244]
     const screen = useScreenSize()
     const zoom = screen === 'small' ? 12 : 17
+
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+    })
+
+    L.Marker.prototype.options.icon = DefaultIcon
 
     return (
         <MapContainer
