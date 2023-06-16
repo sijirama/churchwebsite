c8bb4ee2c62c9a852a64b7b4366b48fd314a878d
sijibomi <gbemilesanmi@gmail.com>
2023-06-16 15:16:47 +0100

    navabr bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import useScreenSize from '../hooks/useScreenSize'
 export default function Navbar() {
     const screen = useScreenSize()
     return (
-        <nav className=" w-full p-4 lg:p-3 bg-transparent flex justify-center items-center bg-mylightgray ">
+        <nav className=" w-full p-4 lg:p-3 flex justify-center items-center bg-mylightgray ">
             <div className="w-1/3 md:w-1/6 lg:w-1/12">
                 {screen === 'small' ? <img src="/fstitleblack.png" /> : <img src="fstitlestackedblack.png" />}
             </div>
