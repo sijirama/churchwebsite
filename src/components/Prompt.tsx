0677e2d6409447e3aba9b7bbc03f809f58bc67fc
sijibomi <gbemilesanmi@gmail.com>
2023-06-17 21:41:18 +0100

    prompt component fix

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,7 +1,7 @@
 export default function Prompt() {
     return (
         <section className="py-14 lg:py-20  flex items-center justify-center bg-white flex-col gap-7 lg:gap-20">
-            <p className="text-black px-11 md:px-24 lg:px-96 font-rubik text-7xl md:text-7xl lg:text-8xl font-extrabold text-center">
+            <p className="text-black px-8 md:px-16 lg:px-96 font-rubik text-6xl md:text-7xl lg:text-8xl font-extrabold text-center">
                 You're a visitor just once, after that you are Family.
             </p>
             <div className="lg:h-1/5 w-1/6 lg:w-1/6 flex items-center justify-center">
