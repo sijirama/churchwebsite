731d6cad791809b09f0fb426772d817af70b4027
sijibomi <gbemilesanmi@gmail.com>
2023-07-06 21:07:28 +0100

    hero change for xl

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section
             style={{ backgroundImage: `url(${Church})` }}
-            className="w-full text-white py-32 md:py-36 lg:py-40 flex flex-col items-center justify-center bg-cover bg-center bg-fixed bg-no-repeat"
+            className="w-full text-white py-32 md:py-36 lg:py-40 xl:py-56 flex flex-col items-center justify-center bg-cover bg-center bg-fixed bg-no-repeat"
         >
             <p className="font-normal md:font-semibold font-manrope text-lg md:text-xl text-center">
                 Welcome to{' '}
