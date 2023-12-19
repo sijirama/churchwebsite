fcc875c916d99108b1ee8188fdfa3c06c1b10de2
sijibomi <gbemilesanmi@gmail.com>
2023-12-19 20:16:11 +0100

    bug:there's a freaking bug in the hero background css

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,23 @@ import Pastor from './components/Pastor'
 import Prompt from './components/Prompt'
 import Services from './components/Services'
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { TextPlugin } from 'gsap/TextPlugin'
+import Lenis from '@studio-freight/lenis'
 export const maintimline = gsap.timeline({})
 
 function App() {
+    // register TextPlugin
+    gsap.registerPlugin(TextPlugin)
+
+    // lenis smooth scrolling config
+    const lenis = new Lenis()
+    lenis.on('scroll', ScrollTrigger.update)
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000)
+    })
+    gsap.ticker.lagSmoothing(0)
+
     return (
         <main>
             <Navbar />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,24 @@
+import { useRef } from 'react'
 import Church from '/church.jpg'
-//import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
+//import { maintimline } from '../App'
+import { BsArrowDown } from 'react-icons/bs'
 
 export default function Hero() {
+    const word = useRef(null)
+    useGSAP()
     return (
         <section
+            id="hero"
             style={{ backgroundImage: `url(${Church})` }}
             className="w-full text-white py-32 md:py-36 lg:py-40 3xl:py-56 flex flex-col items-center justify-center bg-cover bg-center bg-fixed bg-no-repeat"
         >
             <p className="font-normal md:font-semibold font-manrope text-lg md:text-xl text-center">
                 Welcome to{' '}
-                <span className="block my-1 font-extrabold text-4xl md:text-5xl lg:text-7xl lg:max-w-3xl -tracking-wider">
+                <span
+                    ref={word}
+                    className="block my-1 font-extrabold text-4xl md:text-5xl lg:text-7xl lg:max-w-3xl -tracking-wider"
+                >
                     {' '}
                     The Foursquare Gospel Church
                 </span>
@@ -17,13 +26,18 @@ export default function Hero() {
             <p className="md:my-2 lg:my-3 font-manrope font-bold text-lg md:text-xl lg:text-2xl">
                 1004, Victoria Island, Lagos.
             </p>
-            <div className="flex gap-1 md:gap-2 lg:gap-1 items-center justify-center my-5 md:my-4">
+            <div className="flex gap-1 md:gap-0 items-center justify-center my-5 md:my-4 bg-black">
                 <div className="w-1/12">
                     <img src="/mxlr.png" className=" w-2/3 " />
                 </div>
                 <p className="font-semibold md:font-semibold font-manrope text-red-900 text-base lg:text-lg">
                     Join us on <span className="font-semibold underline">Mixlr</span>
                 </p>
+            </div>
+            <div className="flex items-center flex-col gap-2 mt-4">
+                <div id="scroll" className="p-1 rounded-full border-white border-2">
+                    <BsArrowDown className="text-xl font-extrabold" />
+                </div>
             </div>
         </section>
     )
